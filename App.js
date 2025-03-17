c31d045e9326995007280d040bdb61d0d648a2e3
SumithRaj05 <sumithrajpurohit03+1@gmail.com>
2025-03-17 19:38:25 +0530

    added webview

--- App.js
+++ App.js
@@ -21,22 +21,3 @@ const styles = StyleSheet.create({
     marginTop: Constants.statusBarHeight,
   },
 });
-
-// import * as React from "react";
-// import { View, Platform } from "react-native";
-// import { WebView } from "react-native-webview";
-
-// export default class App extends React.Component {
-//   render() {
-//     return Platform.OS === "web" ? (
-//       <iframe src="https://www.somedomain.com/" height={'100%'} width={'100%'} />
-//     ) : (
-//       <View style={{ flex: 1 }}>
-//         <WebView
-//           source={{ uri: "https://www.somedomain.com/" }}
-//           style={{marginTop: 22, flex: 1}}
-//         />
-//       </View>
-//     )
-//   }
-// }
