067f532767cdaddff4ddcfdeac19c7519fdefcb0
SumithRaj05 <sumithrajpurohit03+1@gmail.com>
2025-03-17 19:38:14 +0530

    added webview

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
-import "react-native-gesture-handler";
-
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
-import { useLoadedAssets } from "./hooks/useLoadedAssets";
-import Navigation from "./navigation";
-import { useColorScheme } from "react-native";
+import React from 'react';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import { StyleSheet, Platform } from 'react-native';
 
 export default function App() {
-  const isLoadingComplete = useLoadedAssets();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (Platform.OS === "web" ? (
+    <iframe src="https://bejiness.com" height={'100%'} width={'100%'} />
+  ) : (
+    <WebView
+      style={styles.container}
+      source={{ uri: 'https://bejiness.com' }}
+    />
+  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
+
+// import * as React from "react";
+// import { View, Platform } from "react-native";
+// import { WebView } from "react-native-webview";
+
+// export default class App extends React.Component {
+//   render() {
+//     return Platform.OS === "web" ? (
+//       <iframe src="https://www.somedomain.com/" height={'100%'} width={'100%'} />
+//     ) : (
+//       <View style={{ flex: 1 }}>
+//         <WebView
+//           source={{ uri: "https://www.somedomain.com/" }}
+//           style={{marginTop: 22, flex: 1}}
+//         />
+//       </View>
+//     )
+//   }
+// }
